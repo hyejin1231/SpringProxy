1f6545700bea3a5c93661da15fc898805c14304a
hyejin1231 <phj981231@naver.com>
2022-01-25 08:59:20 +0900

    20220125 Spring 프록시 패턴과 데코레이션 패턴 예제 프로젝트 만들기 V1

---
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -0,0 +1,16 @@
+package hello.proxy;
+
+import hello.proxy.config.AppV1Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록한 것 "hello.proxy.app"만 컴포넌트 스캔하라고 하기 위해서
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
